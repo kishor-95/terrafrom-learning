335cff225b927cdda172304b6755a8cf72f2ed0e
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-01-28 17:37:38 +0530

    task4 added

---
+++ kodekloud-terraform/task5/main.tf
@@ -0,0 +1,19 @@
+variable "cidr_block" {
+  default = "192.168.0.0/24"
+  type    = string
+
+}
+
+variable "name" {
+  default = "devops-vpc"
+  type    = string
+}
+
+resource "aws_vpc" "devops_vpc" {
+  cidr_block = var.cidr_block
+  assign_generated_ipv6_cidr_block = true
+
+  tags = {
+    Name = var.name
+  }
+}
