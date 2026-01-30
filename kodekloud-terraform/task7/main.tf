bbecd7ed66ff92570a52f401ba434231a0d11b3c
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-01-30 12:23:19 +0530

    task7 added

--- kodekloud-terraform/task7/main.tf
+++ kodekloud-terraform/task7/main.tf
@@ -12,17 +12,25 @@ resource "aws_key_pair" "my_key" {
   }
 }  
 
+## default vpc_id
+
+data "aws_vpc" "default" {}
+
 
 ## security Group
 
-data "aws_security_group" "default" {}
+data "aws_security_group" "default" {
+    name = "default"
+    vpc_id = data.aws_vpc.default.id
+}
+
 
 ## ec2 instance
 
 resource "aws_instance" "example" {
         ami = var.ami
         instance_type = var.instance_type
-        vpc_security_group_ids = data.aws_security_group.default.id
+        vpc_security_group_ids = [data.aws_security_group.default.id]
         key_name = aws_key_pair.my_key.id
 
 
