4df95a30cbd16025cd3937533320a4c4379d5591
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-01-30 12:37:35 +0530

    task7 added

--- kodekloud-terraform/task7/main.tf
+++ kodekloud-terraform/task7/main.tf
@@ -6,12 +6,13 @@ resource "tls_private_key" "ssh_key" {
 resource "aws_key_pair" "my_key" {
   key_name   = var.key_name
   public_key = tls_private_key.ssh_key.public_key_openssh
+}
 
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > $(PATH_MOUDLE)/my-key-pair.pem"
-  }
-}  
-
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/${var.key_name}.pem"
+  file_permission = "0400"
+}
 ## default vpc_id
 
 data "aws_vpc" "default" {}
@@ -26,15 +27,14 @@ data "aws_security_group" "default" {
 
 
 ## ec2 instance
-
 resource "aws_instance" "example" {
-        ami = var.ami
-        instance_type = var.instance_type
-        vpc_security_group_ids = [data.aws_security_group.default.id]
-        key_name = aws_key_pair.my_key.id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.default.id]
+  key_name               = aws_key_pair.my_key.key_name
 
-
-    tags = {
-        Name = var.instance_name
-        }
+  tags = {
+    Name = var.instance_name
+  }
 }
+
