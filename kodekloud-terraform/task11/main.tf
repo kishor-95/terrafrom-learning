dd28978cb5177f2891fdf82d26e91df70df185b6
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-02-03 17:07:58 +0530

    task11 added

---
+++ kodekloud-terraform/task11/main.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudwatch_metric_alarm" "nautilus_alarm" {
+  alarm_name                = "nautilus-alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 5
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 1
+  statistic                 = "Average"
+  threshold                 = 80
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
