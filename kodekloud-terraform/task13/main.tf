294a8e4a8f7bfc865feebef21c4e33860f4e85d5
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-02-05 13:20:01 +0530

    task13 added

---
+++ kodekloud-terraform/task13/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "nautilus_s3_private" {
+  bucket = "nautilus-s3-29871"
+}
+
+
+resource "aws_s3_bucket_public_access_block" "Pulic_bucket" {
+  bucket = aws_s3_bucket.nautilus_s3_private.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_address" {
+  value = aws_s3_bucket.nautilus_s3_private.bucket_domain_name
+}
+
