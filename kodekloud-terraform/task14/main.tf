ea7c529ceed0958c867b701a2c0cee60f4d37856
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-02-06 14:39:59 +0530

    task14 added

--- kodekloud-terraform/task14/main.tf
+++ kodekloud-terraform/task14/main.tf
@@ -1,17 +1,3 @@
-resource "aws_s3_bucket" "devops_s3_public" {
-  bucket = "devops-s3-11613"
-}
-
-
-resource "aws_s3_bucket_public_access_block" "Pulic_bucket" {
-  bucket = aws_s3_bucket.devops_s3_public.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-output "bucket_address" {
-  value = aws_s3_bucket.devops_s3_public.bucket_domain_name
+resource "aws_iam_user" "user" {
+    name = "iamuser_javed"
 }
