bc7a8e9abff9f81120cb9b0fc94d98b0358b0d3f
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-02-08 11:01:22 +0530

    Creating IAM policy  using Terraform

--- kodekloud-terraform/task16/main.tf
+++ kodekloud-terraform/task16/main.tf
@@ -1,3 +1,23 @@
-resource "aws_iam_group" "group" {
-    name = "iamgroup_yousuf"
+resource "aws_iam_policy" "policy" {
+  name        = "iampolicy_rose"
+  path        = "/"
+  description = "description = "Read-only access to EC2 console: instances, AMIs, and snapshots"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeImages",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",  
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
