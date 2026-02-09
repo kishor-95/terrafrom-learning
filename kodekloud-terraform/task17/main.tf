98e26676f7820cb6c42238090f63beba330a0874
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-02-09 16:59:00 +0530

    Creating Dynamodb table  using Terraform

--- kodekloud-terraform/task17/main.tf
+++ kodekloud-terraform/task17/main.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "table" {
     name = "devops-user"
     hash_key = "devops_id"
-    billing_mode = "PAY_PER_RESQUEST"
+    billing_mode = "PAY_PER_REQUEST"
 
  attribute {
     name = "UserId"
