73be830ebaf54799d729de0903facf6122c89449
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-02-10 15:29:17 +0530

    Creating AWS Kinesis data stream using Terraform

---
+++ kodekloud-terraform/task18/main.tf
@@ -0,0 +1,9 @@
+resource "aws_kinesis_stream" "test_stream" {
+  name             = "devops-stream"
+  shard_count       = "1"
+
+  tags = {
+    Name = "devops-stream"
+    Environment = "test"
+  }
+}
