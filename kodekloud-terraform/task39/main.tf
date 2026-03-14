ce257406c4f4a21924b97fa77f573c3959853a9d
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-03-14 21:37:40 +0530

    user defined variable  IAM role creation using Terraform

---
+++ kodekloud-terraform/task39/main.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "role" {
+  name = var.KKE_iamrole
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
